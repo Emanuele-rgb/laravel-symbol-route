73e22fdfd68d1216ebe19c7a7bc16550fc8a0a4e
Emanuele Sanquirico <manu.sanquirico@gmail.com>
2020-06-10 19:37:27 +0200

    Setup + test working

---
+++ resources/views/blog/index.blade.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<html lang="en" dir="ltr">
+  <head>
+    <meta charset="utf-8">
+    <title></title>
+  </head>
+  <body>
+    <header>
+      <h1>My blog</h1>
+
+    Ciao
+    </header>
+
+  </body>
+</html>
